f4d4522792ec82c60dd6753845d7f6b1666c4fad
Cinnamon18 <{11944881}+{Cinnamon18}@users.noreply.github.com>
2021-06-12 16:45:40 -0400

    sobs

---
+++ app.py
@@ -0,0 +1,11 @@
+# Just to shut up Azure lmao
+from flask import Flask
+from threading import Thread
+app = Flask(__name__)
+def azureWhy():
+	app.run(port=8000)
+thread = Thread(target=azureWhy)
+thread.start()
+@app.route("/")
+def hello():
+    return "Hello, World!"
--- bot.py
+++ bot.py
@@ -33,18 +33,6 @@ botState.test = False
 
 gacha = Gacha(botState.enabledFranchises)
 
-# Just to shut up Azure lmao
-from flask import Flask
-from threading import Thread
-app = Flask(__name__)
-def azureWhy():
-	app.run(port=8000)
-thread = Thread(target=azureWhy)
-thread.start()
-@app.route("/")
-def hello():
-    return "Hello, World!"
-
 
 
 
