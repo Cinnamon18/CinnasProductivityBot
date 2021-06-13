0809b94bae72c9dc732d9579cba827bc4dadda3e
Cinnamon18 <{11944881}+{Cinnamon18}@users.noreply.github.com>
2021-06-12 22:30:03 -0400

    azure pls

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # Just to shut up Azure lmao
 from flask import Flask
-from threading import Thread
+# from threading import Thread
 app = Flask(__name__)
 def azureWhy():
-	app.run(port=8000)
-thread = Thread(target=azureWhy)
-thread.start()
+	app.run()
+# thread = Thread(target=azureWhy)
+# thread.start()
 @app.route("/")
 def hello():
     return "Hello, World!"
